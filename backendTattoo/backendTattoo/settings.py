9657311267db7653fbdb9ca8e5988124f49fc0f4
Ana María Carrillo <72003315+AMCarrillo@users.noreply.github.com>
2022-02-24 01:19:04 -0600

    Update settings.py

--- backendTattoo/backendTattoo/settings.py
+++ backendTattoo/backendTattoo/settings.py
@@ -136,8 +136,8 @@ USE_TZ = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_HOST_USER="kenyiheral@gmail.com"
-EMAIL_HOST_PASSWORD="trampa07."
+EMAIL_HOST_USER="*****@gmail.com"
+EMAIL_HOST_PASSWORD="******"
 EMAIL_USE_TLS = True
 
 
